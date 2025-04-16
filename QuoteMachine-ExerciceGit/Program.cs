bcbcbc4ebec1976423cf184f643d0f9c3df9eb53
exeloz <christophercoulombe@gmail.com>
2025-04-16 07:32:15 -0400

    00001 : Test

--- QuoteMachine-ExerciceGit/Program.cs
+++ QuoteMachine-ExerciceGit/Program.cs
@@ -21,10 +21,6 @@ static void AddNewQuote(QuoteManager manager)
     Console.WriteLine("[Simulation] On ajouterait une nouvelle citation ici.");
     // Exemple futur :
     // Console.Write("Texte : ");
-    // var texte = Console.ReadLine();
-    // Console.Write("Auteur : ");
-    // var auteur = Console.ReadLine();
-    // manager.AddQuote(texte, auteur);
     // Console.WriteLine("Citation ajoutée !");
 }
 
